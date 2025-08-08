68c7e323799c91d9819bc6685b72730b4e82a6d7
Pekos123 <pawelwojas123@wp.pl>
2025-08-08 18:01:33 +0200

    Adding working SDL

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,45 @@
 #include <SDL3/SDL.h>
 #include <iostream>
 
-int main() {
-    if (SDL_Init(0) != 0) {
+int main(int argc, char* argv[]) {
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
         std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
         return 1;
     }
-    std::cout << "SDL initialized!" << std::endl;
+
+    SDL_Window* window = SDL_CreateWindow(
+        "SDL3 Calculator", // title
+        640, 480,          // w, h
+        0                  // flags (use 0 for default)
+    );
+
+    if (!window) {
+        std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    bool running = true;
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, nullptr);
+
+    while (running) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) {
+                running = false;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+
+        SDL_Delay(16); // To not overload CPU
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
 }
