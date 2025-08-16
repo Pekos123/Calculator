1fb525a1bf8016c151cf9586816af3816afaf8a4
Pekos123 <pawelwojas123@wp.pl>
2025-08-16 15:40:37 +0200

    Adding .gitignore and deleteing build dir

--- build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pablo/Desktop/C++ Projects/learn_cpp/Calculator/tests
-# Build directory: /home/pablo/Desktop/C++ Projects/learn_cpp/Calculator/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(CalculatorUnitTests "/home/pablo/Desktop/C++ Projects/learn_cpp/Calculator/build/tests/calculator_tests")
-set_tests_properties(CalculatorUnitTests PROPERTIES  _BACKTRACE_TRIPLES "/home/pablo/Desktop/C++ Projects/learn_cpp/Calculator/tests/CMakeLists.txt;33;add_test;/home/pablo/Desktop/C++ Projects/learn_cpp/Calculator/tests/CMakeLists.txt;0;")
